5261ea86f0f1d5723382b22403cf73def254a0b6
Nadezhda Kovalenko <95223859+Nadina00@users.noreply.github.com>
2022-11-07 18:31:05 +0200

    add files

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,10 +7,12 @@ const {
   loginCont,
   updateStatusSub,
   currentUser,
+  userAvatar,
   logoutCont,
 } = require("../../controllers/authControllers");
 
 const { tokenMidleware } = require("../../middlewares/authMidleware");
+const { uploadMidleware } = require("../../middlewares/uploadMidleware");
 
 router.post("/signup", registrationCont);
 
@@ -21,5 +23,12 @@ router.get("/current", tokenMidleware, currentUser);
 router.get("/logout", tokenMidleware, logoutCont);
 
 router.patch("/:id", tokenMidleware, updateStatusSub);
+
+router.patch(
+  "/avatars/:id",
+  tokenMidleware,
+  uploadMidleware.single("avatar"),
+  userAvatar
+);
 
 module.exports = router;
